0f1a4abc501cd9ef846ec4c45b50a0bd4f862fd0
eidanfili <anning3232@gmail.com>
2022-05-10 21:27:12 -0600

    added button conditionals

--- src/homework/App.js
+++ src/homework/App.js
@@ -5,6 +5,18 @@ function App() {
   const [count, setCount] = useState(0);
 
   const conditional = () => {
+    if (count <= -9) {
+      document.querySelector(".btn-dwn").disabled = true;
+    } else if (count >= -10) {
+      document.querySelector(".btn-dwn").disabled = false;
+    }
+    if (count <= 9) {
+      document.querySelector(".btn-up").disabled = false;
+    } else if (count >= 10) {
+      document.querySelector(".btn-up").disabled = true;
+    }
+  };
+  const isPositiveOrNot = () => {
     if (count < 0) {
       return <div>your element is negative</div>;
     } else {
@@ -15,9 +27,14 @@ function App() {
   return (
     <div className="App">
       <h1>You clicked {count} times</h1>
-      <button onClick={() => setCount(count + 1)}>Click me to go up</button>
-      <button onClick={() => setCount(count - 1)}>Click me to go down</button>
+      <button className="btn-up" onClick={() => setCount(count + 1)}>
+        Click me to go up
+      </button>
+      <button className="btn-dwn" onClick={() => setCount(count - 1)}>
+        Click me to go down
+      </button>
       {conditional()}
+      {isPositiveOrNot()}
     </div>
   );
 }

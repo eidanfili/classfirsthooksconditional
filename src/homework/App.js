39afd324b78cfb9f1afa7f8c2c205f5b54cd4f06
eidanfili <anning3232@gmail.com>
2022-05-10 21:39:04 -0600

    added bouncer

--- src/homework/App.js
+++ src/homework/App.js
@@ -3,24 +3,35 @@ import "./App.scss";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
-  const conditional = () => {
-    if (count <= -9) {
-      document.querySelector(".btn-dwn").disabled = true;
-    } else if (count >= -10) {
-      document.querySelector(".btn-dwn").disabled = false;
-    }
-    if (count <= 9) {
-      document.querySelector(".btn-up").disabled = false;
-    } else if (count >= 10) {
-      document.querySelector(".btn-up").disabled = true;
-    }
-  };
+  // const conditional = () => {
+  //   if (count <= -9) {
+  //     document.querySelector(".btn-dwn").disabled = true;
+  //   } else if (count >= -10) {
+  //     document.querySelector(".btn-dwn").disabled = false;
+  //   }
+  //   if (count <= 9) {
+  //     document.querySelector(".btn-up").disabled = false;
+  //   } else if (count >= 10) {
+  //     document.querySelector(".btn-up").disabled = true;
+  //   }
+  // };
   const isPositiveOrNot = () => {
     if (count < 0) {
       return <div>your element is negative</div>;
     } else {
       return <div>your element is positive</div>;
+    }
+  };
+
+  const bouncer = () => {
+    if (name.toLowerCase() === "eidan") {
+      return <div>omg the mvp</div>;
+    } else if (name.toLowerCase() === "something cool") {
+      return <div>sure get in</div>;
+    } else {
+      return <div>get out</div>;
     }
   };
 
@@ -33,8 +44,13 @@ function App() {
       <button className="btn-dwn" onClick={() => setCount(count - 1)}>
         Click me to go down
       </button>
-      {conditional()}
+      {/* {conditional()} */}
       {isPositiveOrNot()}
+
+      <div>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+      </div>
+      {bouncer()}
     </div>
   );
 }
